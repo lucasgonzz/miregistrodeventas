2703ae54bba2779c4c2c0104f77d148342ad4320
=Lucas <=lucasgonzalez5500@gmail.com>
2019-10-30 18:10:33 -0300

    Terminada la vista de codigos de barras
    Empiezo con las vistas de ventas de mayoristas

--- resources/views/common/codigos-de-barra.blade.php
+++ resources/views/common/codigos-de-barra.blade.php
@@ -10,22 +10,29 @@
 				</div>
 				<form>
 					<div class="card-body">
-						<h5 class="card-title">Genere un pdf con los codigos de barra y la cantidad que necesite</h5>
+						<!-- <h5 class="card-title">Genere un pdf con los codigos de barra y la cantidad que necesite</h5> -->
 						<div class="row">
 							<div class="col-12 col-lg-6">
-								<form action="">
-									<div class="form-group">
-										<label for="bar-code">Nuevo codigo de barras</label>
-										<input type="text" placeholder="Ingrese su nuevo codigo de barras" class="form-control">
+								<div class="card">
+									<div class="card-header">
+										Genere un nuevo codigo de barras
 									</div>
-									<div class="form-group">
-										<label for="bar-code">Cuantas veces necesita imprimirlo</label>
-										<input type="text" placeholder="Cantidad de veces que se imprimira" class="form-control">
+									<div class="card-body">
+										<form action="">
+											<div class="form-group">
+												<label for="bar-code">Nuevo codigo de barras</label>
+												<input type="text" placeholder="Ingrese su nuevo codigo de barras" class="form-control">
+											</div>
+											<div class="form-group">
+												<label for="bar-code">Cuantas veces necesita imprimirlo</label>
+												<input type="number" min="0" placeholder="Cantidad de veces que se imprimira" class="form-control">
+											</div>
+										</form>
 									</div>
-									<div class="form-group">
+									<div class="card-footer">
 										<button class="btn btn-primary">Generar codigo</button>
 									</div>
-								</form>
+								</div>
 							</div>
 							<div class="col-12 col-lg-6">
 								<ul class="list-group list-bar-codes">
